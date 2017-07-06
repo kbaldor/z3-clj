aa497f5e7214ca83102f7127f1f8e730c434803b
Kevin Baldor <kbaldor@anduril.electro.swri.edu>
2017-07-06 17:49:23 -0500

    Added with-context macro for setting and then cleaning up the context.

--- src/z3_clj/core.clj
+++ src/z3_clj/core.clj
@@ -3,22 +3,53 @@
   (:import [com.microsoft.z3 Version Context ArithExpr BoolExpr
             Status])
   (:require [clojure.reflect :as r])
-  (:use [clojure.pprint :only [print-table]]))
+  (:use [clojure.pprint :only [pprint print-table]]))
 
 (def ^:dynamic *context* nil)
 
 (defn context [& args] 
   (Context. (into {} (partition 2 args))))
 
+(defn- ->string [thing]
+  (cond
+   (symbol? thing)  (name thing)
+   (keyword? thing) (name thing)
+   :else            (str thing)))
+
+(defmacro with-context [params & body]
+  (let [params (into [] (map ->string params))]
+    `(let [new-context# (Context. (into {} (map (partial apply vector) (partition 2 ~params))))
+           result#      (binding [*context* new-context#] ~@body)
+           result#      (if (sequential? result#) (doall result#) result#)]
+           (.close new-context#)
+           result#)))
+
 (defn satisfiable? [status]
   (clojure.core/= status Status/SATISFIABLE))
 
-;; Arithmetic expressions
+;; TODO: Handle the creation of new Sorts
 
-(defn int [identifier-or-value]
+;; constants and values
+
+(defn bool [identifier-or-value]
   (if (string? identifier-or-value)
-    (.mkIntConst *context* identifier-or-value)
-    (.mkInt      *context* identifier-or-value)))
+    (.mkBoolConst *context* identifier-or-value)
+    (.mkBool      *context* identifier-or-value)))
+
+(defn real [identifier-or-value]
+  (if (string? identifier-or-value)
+    (.mkRealConst *context* identifier-or-value)
+    (.mkReal      *context* identifier-or-value)))
+
+(defn int 
+  ([identifier-or-value]
+   (if (string? identifier-or-value)
+     (.mkIntConst *context* identifier-or-value)
+     (.mkInt      *context* identifier-or-value)))
+  ([identifier size]
+   (.mkBVConst *context* identifier size)))
+
+;; Arithmetic expressions
 
 (defn + [& arithmetic-expressions]
   (.mkAdd *context* (into-array ArithExpr arithmetic-expressions)))
--- test/z3_clj/core_test.clj
+++ test/z3_clj/core_test.clj
@@ -2,7 +2,9 @@
   (:require [clojure.test :refer :all]
             [z3-clj.core :as z3]))
 
-(binding [z3/*context* (z3/context ["model" "true"])]
+
+#_(println 
+    (z3/with-context [:model :true]
              (let [xExp (z3/int "x")
                    yExp (z3/int "y")
                    opt  (z3/optimizer (z3/= (z3/+ xExp yExp) (z3/int 10))
@@ -12,12 +14,12 @@
                    my   (z3/maximize opt yExp)
                    status (z3/check opt)]
                (if (z3/satisfiable? status)
-                 (map #(-> % .getValue .getInt64) [mx my]))))
+                 (map #(-> % .getValue .getInt64) [mx my])))))
 
 (deftest opt-test
   (testing "Optimizer"
     (is (= [10 0]
-           (binding [z3/*context* (z3/context ["model" "true"])]
+           (z3/with-context [:model :true]
              (let [xExp (z3/int "x")
                    yExp (z3/int "y")
                    opt  (z3/optimizer (z3/= (z3/+ xExp yExp) (z3/int 10))
